9a7cff80f63744de44290d424f34fdd5d8562559
Robert Wise <robcodehub@gmail.com>
2019-10-12 21:27:22 +0700

    started pre-work

---
+++ test/spec.js
@@ -0,0 +1,10 @@
+describe('Authentication', () => {
+  describe('GET /api/sessions', ()=> {
+    describe('When not logged in', () => {
+      it('returns a 401', async() => {
+        const response = await app.get('/api/sessions');
+        expect(response.status).to.equal(401);
+      })
+    })
+  })
+});
